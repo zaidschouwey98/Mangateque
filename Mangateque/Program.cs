225ada78af8906c2a2d9b73f97d498f50a408677
zaidschouwey98 <zazar981@hotmail.com>
2022-02-17 13:14:05 +0100

    Add route

--- Mangateque/Program.cs
+++ Mangateque/Program.cs
@@ -41,8 +41,14 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+    endpoints.MapRazorPages();
+
+});
 
 app.Run();
