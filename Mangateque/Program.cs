82ebd902f3a566d7fb70ff04d13f4d4fd87c2f4f
zaidschouwey98 <zazar981@hotmail.com>
2022-02-17 10:22:11 +0100

    Initial commit

--- Mangateque/Program.cs
+++ Mangateque/Program.cs
@@ -1,7 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Mangateque.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<mangatekContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(builder.Configuration.GetConnectionString("MvcMangaContext"), new MySqlServerVersion(new Version(8, 0, 27)))
+// The following three options help with debugging, but should
+// be changed or removed for production.
+);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
